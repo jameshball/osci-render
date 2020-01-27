51a3688548767ec4d3a4c5fa15f8f8c56b4d9635
James Ball <balljames63@gmail.com>
2020-01-27 21:39:46 +0000

    Added .equals to point and line and created testsuite

--- src/Line.java
+++ src/Line.java
@@ -7,20 +7,20 @@ public class Line {
     this.b = b;
   }
 
-  public Line(float x1, float y1, float x2, float y2) {
+  public Line(double x1, double y1, double x2, double y2) {
     this.a = new Point(x1, y1);
     this.b = new Point(x2, y2);
   }
 
-  public float length() {
-    return (float) Math.sqrt(Math.pow(getX1() - getX2(), 2) + Math.pow(getY1() - getY2(), 2));
+  public double length() {
+    return Math.sqrt(Math.pow(getX1() - getX2(), 2) + Math.pow(getY1() - getY2(), 2));
   }
 
   public void rotate(double theta) {
-    setX1((float) (getX1() * Math.cos(theta) - getY1() * Math.sin(theta)));
-    setY1((float) (getX1() * Math.sin(theta) + getY1() * Math.cos(theta)));
-    setX2((float) (getX2() * Math.cos(theta) - getY2() * Math.sin(theta)));
-    setY2((float) (getX2() * Math.sin(theta) + getY2() * Math.cos(theta)));
+    setX1(getX1() * Math.cos(theta) - getY1() * Math.sin(theta));
+    setY1(getX1() * Math.sin(theta) + getY1() * Math.cos(theta));
+    setX2(getX2() * Math.cos(theta) - getY2() * Math.sin(theta));
+    setY2(getX2() * Math.sin(theta) + getY2() * Math.cos(theta));
   }
 
   public Point getA() {
@@ -31,35 +31,48 @@ public class Line {
     return b;
   }
 
-  public float getX1() {
+  public double getX1() {
     return a.getX();
   }
 
-  public float getY1() {
+  public double getY1() {
     return a.getY();
   }
 
-  public float getX2() {
+  public double getX2() {
     return b.getX();
   }
 
-  public float getY2() {
+  public double getY2() {
     return b.getY();
   }
 
-  public void setX1(float x1) {
+  public void setX1(double x1) {
     a.setX(x1);
   }
 
-  public void setY1(float y1) {
+  public void setY1(double y1) {
     a.setY(y1);
   }
 
-  public void setX2(float x2) {
+  public void setX2(double x2) {
     b.setX(x2);
   }
 
-  public void setY2(float y2) {
+  public void setY2(double y2) {
     b.setY(y2);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
+    } else if (obj instanceof Line) {
+      Line line = (Line) obj;
+
+      return line.getA().equals(getA()) && line.getB().equals(getB());
+    } else {
+      return false;
+    }
   }
 }
