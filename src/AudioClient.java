8bf4eb7613590baa9ce7f41e4de6e23b3afac578
James Ball <balljames63@gmail.com>
2020-01-29 20:34:02 +0000

    Added support for drawing 'polygrams'

--- src/AudioClient.java
+++ src/AudioClient.java
@@ -11,32 +11,16 @@ public class AudioClient {
     AudioPlayer.FORMAT = new XtFormat(new XtMix(sampleRate, XtSample.FLOAT32), 0, 0, 2, 0);
 
     for (int i = 2; i < 7; i++) {
-      AudioPlayer.addLines(generatePolygon(i, 0.5));
+      AudioPlayer.addLines(Shapes.generatePolygon(i, 0.5));
     }
-    AudioPlayer.setRotateSpeed(0.4);
+
+    //AudioPlayer.addLines(Shapes.generatePolygram(5, 4, new Point(0, 0.4).rotate(Math.PI / 5)));
+
+    AudioPlayer.setRotateSpeed(0.8);
 
     player.start();
     while (true) {
 
     }
-  }
-
-  public static List<Line> generatePolygon(int sides, double scale) {
-    List<Line> polygon = new ArrayList<>();
-
-    double theta = 2 * Math.PI / sides;
-
-    Point start = new Point(scale, scale);
-    Point rotated = start.rotate(theta);
-
-    polygon.add(new Line(start, rotated));
-
-    while (!rotated.equals(start)) {
-      polygon.add(new Line(rotated.copy(), rotated.rotate(theta)));
-
-      rotated = rotated.rotate(theta);
-    }
-
-    return polygon;
   }
 }
