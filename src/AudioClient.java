92e1d80692e4502fae2c72ca6ca7846359141d2c
James Ball <balljames63@gmail.com>
2020-01-29 20:45:06 +0000

    Cleaned code

--- src/AudioClient.java
+++ src/AudioClient.java
@@ -1,26 +1,17 @@
 import com.xtaudio.xt.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class AudioClient {
-  public static void main(String[] args) throws Exception {
-    int sampleRate = 192000;
+  private static final int SAMPLE_RATE = 192000;
 
+  public static void main(String[] args) {
     AudioPlayer player = new AudioPlayer();
-    AudioPlayer.FORMAT = new XtFormat(new XtMix(sampleRate, XtSample.FLOAT32), 0, 0, 2, 0);
+    AudioPlayer.FORMAT = new XtFormat(new XtMix(SAMPLE_RATE, XtSample.FLOAT32), 0, 0, 2, 0);
 
-    for (int i = 2; i < 7; i++) {
-      AudioPlayer.addLines(Shapes.generatePolygon(i, 0.5));
-    }
+    AudioPlayer.addLines(Shapes.generatePolygon(100, 0.5));
+    AudioPlayer.addLines(Shapes.generatePolygram(5, 3, 0.5));
 
-    //AudioPlayer.addLines(Shapes.generatePolygram(5, 4, new Point(0, 0.4).rotate(Math.PI / 5)));
-
-    AudioPlayer.setRotateSpeed(0.8);
+    AudioPlayer.setRotateSpeed(0.4);
 
     player.start();
-    while (true) {
-
-    }
   }
 }
