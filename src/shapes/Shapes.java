bce1e4e843920fc9e0ad2775dc504daff429cf5d
James Ball <balljames63@gmail.com>
2020-02-02 21:21:36 +0000

    Renamed Point to Vector and added translate functions

--- src/shapes/Shapes.java
+++ src/shapes/Shapes.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Shapes {
-  public static List<Line> generatePolygram(int sides, int angleJump, Point start, double weight) {
+  public static List<Line> generatePolygram(int sides, int angleJump, Vector start, double weight) {
     List<Line> polygon = new ArrayList<>();
 
     double theta = angleJump * 2 * Math.PI / sides;
 
-    Point rotated = start.rotate(theta);
+    Vector rotated = start.rotate(theta);
 
     polygon.add(new Line(start, rotated, weight));
 
@@ -22,32 +22,32 @@ public class Shapes {
     return polygon;
   }
 
-  public static List<Line> generatePolygram(int sides, int angleJump, Point start) {
+  public static List<Line> generatePolygram(int sides, int angleJump, Vector start) {
     return generatePolygram(sides, angleJump, start, Line.DEFAULT_WEIGHT);
   }
 
   public static List<Line> generatePolygram(int sides, int angleJump, double scale, double weight) {
-    return generatePolygram(sides, angleJump, new Point(scale, scale), weight);
+    return generatePolygram(sides, angleJump, new Vector(scale, scale), weight);
   }
 
   public static List<Line> generatePolygram(int sides, int angleJump, double scale) {
-    return generatePolygram(sides, angleJump, new Point(scale, scale));
+    return generatePolygram(sides, angleJump, new Vector(scale, scale));
   }
 
-  public static List<Line> generatePolygon(int sides, Point start, double weight) {
+  public static List<Line> generatePolygon(int sides, Vector start, double weight) {
     return generatePolygram(sides, 1, start, weight);
   }
 
-  public static List<Line> generatePolygon(int sides, Point start) {
+  public static List<Line> generatePolygon(int sides, Vector start) {
     return generatePolygram(sides, 1, start);
   }
 
   public static List<Line> generatePolygon(int sides, double scale, double weight) {
-    return generatePolygon(sides, new Point(scale, scale), weight);
+    return generatePolygon(sides, new Vector(scale, scale), weight);
   }
 
   public static List<Line> generatePolygon(int sides, double scale) {
-    return generatePolygon(sides, new Point(scale, scale));
+    return generatePolygon(sides, new Vector(scale, scale));
   }
 
   public static List<Line> cleanupLines(List<Line> lines) {
