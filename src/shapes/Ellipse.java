4d558900dc980ba682fbf02f74f350811c2c7343
James Ball <balljames63@gmail.com>
2020-02-05 23:55:02 +0000

    Added new constructors for Ellipse

--- src/shapes/Ellipse.java
+++ src/shapes/Ellipse.java
@@ -5,27 +5,33 @@ public class Ellipse implements Shape {
   private final double b;
   private final double weight;
   private final double length;
+  private final Vector position;
 
-  public Ellipse(double a, double b, double weight) {
+  public Ellipse(double a, double b, double weight, Vector position) {
     this.a = a;
     this.b = b;
     this.weight = weight;
+    this.position = position;
     // Approximation of length.
     this.length = 2 * Math.PI * Math.sqrt((this.a * this.a + this.b * this.b) / 2);
   }
 
+  public Ellipse(double a, double b, Vector position) {
+    this(a, b, 100, position);
+  }
+
   public Ellipse(double a, double b) {
-    this(a, b, 100);
+    this(a, b, 100, new Vector());
   }
 
   @Override
   public float nextX(double drawingProgress) {
-    return (float) (a * Math.sin(2 * Math.PI * drawingProgress));
+    return (float) (position.getX() + a * Math.sin(2 * Math.PI * drawingProgress));
   }
 
   @Override
   public float nextY(double drawingProgress) {
-    return (float) (b * Math.cos(2 * Math.PI * drawingProgress));
+    return (float) (position.getY() + b * Math.cos(2 * Math.PI * drawingProgress));
   }
 
   // TODO: Implement ellipse rotation.
@@ -36,13 +42,13 @@ public class Ellipse implements Shape {
 
   @Override
   public Shape scale(double factor) {
-    return new Ellipse(a * factor, b * factor, weight);
+    return new Ellipse(a * factor, b * factor, weight, position.scale(factor));
   }
 
   // TODO: Implement ellipse translation.
   @Override
   public Shape translate(Vector vector) {
-    return this;
+    return new Ellipse(a, b, weight, position.add(vector));
   }
 
   @Override
