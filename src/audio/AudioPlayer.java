b7712d3e36a2dd6da7c77d17e146acac530357fc
James Ball <balljames63@gmail.com>
2020-02-12 16:02:13 +0000

    Used library for reading obj files instead for compatibility

--- src/audio/AudioPlayer.java
+++ src/audio/AudioPlayer.java
@@ -21,7 +21,7 @@ public class AudioPlayer extends Thread {
   private static double TRANSLATE_SPEED = 0;
   private static Vector2 TRANSLATE_VECTOR;
   private static final int TRANSLATE_PHASE_INDEX = 0;
-  private static double ROTATE_SPEED = 0.4;
+  private static double ROTATE_SPEED = 0;
   private static final int ROTATE_PHASE_INDEX = 1;
   private static double SCALE = 1;
 
--- src/engine/Mesh.java
+++ src/engine/Mesh.java
@@ -1,16 +1,14 @@
 package engine;
 
-import java.io.File;
+import com.mokiat.data.front.parser.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.util.regex.MatchResult;
-import java.util.stream.Collectors;
 
 public class Mesh {
-  private static final String VERTEX_PATTERN = "(?m)^v .*";
-  private static final String FACE_PATTERN = "(?m)^f .*";
-
   private List<Vector3> vertices;
   private List<Integer> edgeData;
 
@@ -32,52 +30,37 @@ public class Mesh {
   }
 
   public static Mesh loadFromFile(String filename) {
-    Scanner sc;
+    List<Vector3> vertices = new ArrayList<>();
+    List<Integer> edgeData = new ArrayList<>();
 
-    try {
-      sc = new Scanner(new File(filename));
-    } catch (Exception e) {
+    // Open a stream to your OBJ resource
+    try (InputStream in = new FileInputStream(filename)) {
+      // Create an OBJParser and parse the resource
+      final IOBJParser parser = new OBJParser();
+      final OBJModel model = parser.parse(in);
+
+      for (OBJVertex vertex : model.getVertices()) {
+        vertices.add(new Vector3(vertex.x, vertex.y, vertex.z));
+      }
+
+      for (OBJObject object : model.getObjects()) {
+        for (OBJMesh mesh : object.getMeshes()) {
+          for (OBJFace face : mesh.getFaces()) {
+            List<OBJDataReference> references = face.getReferences();
+
+            for (int i = 0; i < references.size(); i++) {
+              edgeData.add(references.get(i).vertexIndex);
+              edgeData.add(references.get((i + 1) % references.size()).vertexIndex);
+            }
+          }
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
       System.err.println("Cannot load mesh data from: " + filename);
       return new Mesh();
     }
-    // load vertices
-    List<Vector3> vertices =
-      sc.findAll(VERTEX_PATTERN)
-      .map(s -> parseVertex(s.group(0)))
-      .collect(Collectors.toList());
-    // load edge data
-    List<Integer> edgeData = new ArrayList<>();
-
-    for(MatchResult result : sc.findAll(FACE_PATTERN).collect(Collectors.toList())) {
-      List<Integer> indices = parseFace(result.group(0));
-
-      for(int i = 0; i < indices.size(); i++ ) {
-        edgeData.add(indices.get(i));
-        edgeData.add(indices.get((i + 1) % indices.size()));
-      }
-    }
 
     return new Mesh(vertices, edgeData);
-  }
-
-  private static Vector3 parseVertex(String data) {
-    String[] coords = data.split(" ");
-
-    double x = Double.parseDouble(coords[1]);
-    double y = Double.parseDouble(coords[2]);
-    double z = Double.parseDouble(coords[3]);
-
-    return new Vector3(x, y, z);
-  }
-
-  private static List<Integer> parseFace(String data) {
-    List<Integer> indices = new ArrayList<>();
-    String[] datas = data.split(" ");
-
-    for(int i = 1; i < datas.length; i++) {
-      indices.add(Integer.parseInt(datas[i].split("/")[0]) - 1);
-    }
-
-    return indices;
   }
 }
