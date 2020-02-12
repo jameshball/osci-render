89ffa78655eb0a3b4f50a7ce92807c58c2974a2e
James Ball <balljames63@gmail.com>
2020-02-12 16:30:16 +0000

    Added todos

--- src/audio/AudioClient.java
+++ src/audio/AudioClient.java
@@ -10,9 +10,15 @@ public class AudioClient {
   private static final double FRAMERATE = 30;
 
   public static void main(String[] args) throws InterruptedException {
+    // TODO: Implement adaptive default weight (i.e. the more things drawn, the lower the weight of lines)
+    //  Calculate weight of lines using depth.
+    //  Reduce weight of lines drawn multiple times.
+    //  Find intersections of lines to (possibly) improve line cleanup.
+    //  Improve performance of line cleanup with a heuristic.
+
     AudioPlayer player = new AudioPlayer(SAMPLE_RATE, 440);
 
-    Camera camera = new Camera(0.6, new Vector3(0, 0, -2));
+    Camera camera = new Camera(0.6, new Vector3(0, 0, -3));
     WorldObject cube = new WorldObject(args[0], new Vector3(0, 0, 0), new Vector3());
     Vector3 rotation = new Vector3(0,Math.PI / 100,Math.PI / 100);
 
