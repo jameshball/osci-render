7b7e3b8464d3e65f5b31bd5c621e949f63e2b2d9
James Ball <james@ball.sh>
2020-10-18 14:29:47 +0100

    Heavy refactor of main function

--- src/audio/AudioClient.java
+++ src/audio/AudioClient.java
@@ -12,6 +12,7 @@ import shapes.Shapes;
 import shapes.Vector2;
 
 public class AudioClient {
+
   private static final int SAMPLE_RATE = 192000;
   private static double OBJ_ROTATE_SPEED = Math.PI / 1000;
   private static final float ROTATE_SPEED = 0;
@@ -30,32 +31,38 @@ public class AudioClient {
   //
   // example:
   // osci-render models/cube.obj 1 0 0 -3 10
-  public static void main(String[] args) {
+  public static void main(String[] programArgs) {
     // TODO: Calculate weight of lines using depth.
     //  Reduce weight of lines drawn multiple times.
     //  Find intersections of lines to (possibly) improve line cleanup.
     //  Improve performance of line cleanup with a heuristic.
 
-    String objFilePath = args[0];
-    float focalLength = Float.parseFloat(args[1]);
-    float cameraX = Float.parseFloat(args[2]);
-    float cameraY = Float.parseFloat(args[3]);
-    float cameraZ = Float.parseFloat(args[4]);
+    AudioArgs args = new AudioArgs(programArgs);
 
-    OBJ_ROTATE_SPEED *= Double.parseDouble(args[5]);
+    OBJ_ROTATE_SPEED *= args.rotateSpeed();
 
-    int numFrames = (int) (2 * Math.PI / OBJ_ROTATE_SPEED);
-
-    Camera camera = new Camera(focalLength, new Vector3(cameraX, cameraY, cameraZ));
-    WorldObject object = new WorldObject(objFilePath);
+    Vector3 cameraPos = new Vector3(args.cameraX(), args.cameraY(), args.cameraZ());
+    Camera camera = new Camera(args.focalLength(), cameraPos);
+    WorldObject object = new WorldObject(args.objFilePath());
     Vector3 rotation = new Vector3(0, OBJ_ROTATE_SPEED, OBJ_ROTATE_SPEED);
+
+    System.out.println("Begin pre-render...");
+    List<List<? extends Shape>> frames = preRender(object, rotation, camera);
+    System.out.println("Finish pre-render");
+    System.out.println("Connecting to audio player");
+    AudioPlayer player = new AudioPlayer(SAMPLE_RATE, frames, ROTATE_SPEED, TRANSLATION_SPEED, TRANSLATION, SCALE, WEIGHT);
+    System.out.println("Starting audio stream");
+    player.play();
+  }
+
+  private static List<List<? extends Shape>> preRender(WorldObject object, Vector3 rotation, Camera camera) {
     List<List<? extends Shape>> preRenderedFrames = new ArrayList<>();
+    int numFrames = (int) (2 * Math.PI / OBJ_ROTATE_SPEED);
 
     for (int i = 0; i < numFrames; i++) {
       preRenderedFrames.add(new ArrayList<>());
     }
 
-    System.out.println("Begin pre-render...");
     AtomicInteger renderedFrames = new AtomicInteger();
 
     // pre-renders the WorldObject in parallel
@@ -70,16 +77,6 @@ public class AudioClient {
       }
     });
 
-    System.out.println("Finish pre-render");
-
-    System.out.println("Connecting to audio player");
-    AudioPlayer player = new AudioPlayer(SAMPLE_RATE, preRenderedFrames);
-    player.setRotateSpeed(ROTATE_SPEED);
-    player.setTranslation(TRANSLATION_SPEED, TRANSLATION);
-    player.setScale(SCALE);
-    player.setWeight(WEIGHT);
-
-    System.out.println("Starting audio stream");
-    player.start();
+    return preRenderedFrames;
   }
 }
