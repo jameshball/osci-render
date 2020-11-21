7ddee3aeeaf21d9bdf6b854127da4774b05ed9ba
James Ball <jhb119@ic.ac.uk>
2020-11-21 17:10:47 +0000

    Return empty list in the case where character glyphs are not found

--- src/parser/TextParser.java
+++ src/parser/TextParser.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import javax.swing.table.DefaultTableCellRenderer;
 import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
 import shapes.Shape;
