18419567d8ecc19e1f18479c343ad2750d7a5140
James Ball <jhb119@ic.ac.uk>
2020-11-22 18:03:20 +0000

    Add slider and basic listener to gui

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,4 +1,22 @@
 package gui;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Slider;
+import javafx.scene.text.Text;
+
+public class Controller implements Initializable {
+
+  @FXML
+  private Text sliderOutput;
+  @FXML
+  private Slider slider;
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    slider.valueProperty().addListener(
+        (source, oldValue, newValue) -> sliderOutput.setText(String.valueOf(slider.getValue())));
+  }
 }
--- src/gui/Main.java
+++ src/gui/Main.java
@@ -8,16 +8,15 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("osci-render.fxml"));
+    primaryStage.setTitle("osci-render");
+    primaryStage.setScene(new Scene(root));
+    primaryStage.show();
+  }
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
