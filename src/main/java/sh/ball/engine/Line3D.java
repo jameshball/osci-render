38bab2105006b0af8ffa92c13591f8b529c59143
James Ball <jhb119@ic.ac.uk>
2020-12-04 20:30:46 +0000

    Complete project standardisation overhaul

---
+++ src/main/java/sh/ball/engine/Line3D.java
@@ -0,0 +1,47 @@
+package sh.ball.engine;
+
+import java.util.Objects;
+
+public class Line3D {
+
+  private final Vector3 start;
+  private final Vector3 end;
+
+  public Line3D(Vector3 start, Vector3 end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  public Vector3 getStart() {
+    return start;
+  }
+
+  public Vector3 getEnd() {
+    return end;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Line3D line3D = (Line3D) o;
+    return Objects.equals(start, line3D.start) && Objects.equals(end, line3D.end);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(start, end);
+  }
+
+  @Override
+  public String toString() {
+    return "Line3D{" +
+        "start=" + start +
+        ", end=" + end +
+        '}';
+  }
+}
---
+++ src/main/java/sh/ball/gui/Gui.java
@@ -0,0 +1,36 @@
+package sh.ball.gui;
+
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Gui extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/osci-render.fxml"));
+    Parent root = loader.load();
+    Controller controller = loader.getController();
+    controller.setStage(primaryStage);
+
+    primaryStage.setTitle("osci-render");
+    primaryStage.setScene(new Scene(root));
+
+    controller.setStage(primaryStage);
+
+    primaryStage.show();
+
+    primaryStage.setOnCloseRequest(t -> {
+      Platform.exit();
+      System.exit(0);
+    });
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
