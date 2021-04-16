146ccb0e7cd8511745e1402bbae9c663eb69d512
James Ball <james@ball.sh>
2021-04-16 18:56:17 +0100

    Abstract AudioPlayer into Renderer to reduce coupling

---
+++ src/main/java/sh/ball/MovableRenderer.java
@@ -0,0 +1,8 @@
+package sh.ball;
+
+public interface MovableRenderer<S, T> extends Renderer<S> {
+  void setRotationSpeed(double speed);
+  void setTranslation(T translation);
+  void setTranslationSpeed(double speed);
+  void setScale(double scale);
+}
