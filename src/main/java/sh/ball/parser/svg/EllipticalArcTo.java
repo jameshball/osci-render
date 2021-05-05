0fdd144f24b5d3a4839c3692b87accb0c314259e
James Ball <james@ball.sh>
2021-05-05 20:59:24 +0100

    Partially implement Elliptical Arcs for SVGs

--- src/main/java/sh/ball/parser/svg/EllipticalArcTo.java
+++ src/main/java/sh/ball/parser/svg/EllipticalArcTo.java
@@ -1,28 +1,136 @@
 package sh.ball.parser.svg;
 
+import java.awt.geom.Arc2D;
 import java.util.ArrayList;
 import java.util.List;
 
+import sh.ball.shapes.Line;
 import sh.ball.shapes.Shape;
+import sh.ball.shapes.Vector2;
 
 class EllipticalArcTo {
 
-  private static List<Shape> parseEllipticalArc(SvgState state, List<Float> args, boolean isAbsolute) {
-    // TODO: Properly implement
+  private static final int EXPECTED_ARGS = 7;
 
-    if (args.size() % 7 != 0 || args.size() < 7) {
+  private static List<Shape> parseEllipticalArc(SvgState state, List<Float> args, boolean isAbsolute) {
+    if (args.size() % EXPECTED_ARGS != 0 || args.size() < EXPECTED_ARGS) {
       throw new IllegalArgumentException(
         "SVG elliptical arc command has incorrect number of arguments.");
     }
 
-    List<Float> lineToArgs = new ArrayList<>();
+    List<Shape> arcs = new ArrayList<>();
 
-    for (int i = 0; i < args.size(); i += 7) {
-      lineToArgs.add(args.get(i + 5));
-      lineToArgs.add(args.get(i + 6));
+    for (int i = 0; i < args.size(); i += EXPECTED_ARGS) {
+      Vector2 newPoint = new Vector2(args.get(i + 5), args.get(i + 6));
+
+      newPoint = isAbsolute ? newPoint : state.currPoint.translate(newPoint);
+
+      System.out.println(args.get(i + 2));
+
+      arcs.addAll(createArc(
+        state.currPoint,
+        args.get(i),
+        args.get(i + 1),
+        args.get(i + 2),
+        args.get(i + 3) == 1,
+        args.get(i + 4) == 1,
+        newPoint
+      ));
+
+      state.currPoint = newPoint;
     }
 
-    return isAbsolute ? LineTo.absolute(state, lineToArgs) : LineTo.relative(state, lineToArgs);
+    return arcs;
+  }
+
+  // The following algorithm is completely based on https://www.w3.org/TR/SVG11/implnote.html#ArcImplementationNotes
+  private static List<Shape> createArc(Vector2 start, double rx, double ry, float theta, boolean largeArcFlag, boolean sweepFlag, Vector2 end) {
+    double x2 = end.getX();
+    double y2 = end.getY();
+    // Ensure radii are valid
+    if (rx == 0 || ry == 0) {
+      return List.of(new Line(start, end));
+    }
+    double x0 = start.getX();
+    double y0 = start.getY();
+    // Compute the half distance between the current and the final point
+    double dx2 = (x0 - x2) / 2.0f;
+    double dy2 = (y0 - y2) / 2.0f;
+    // Convert theta from degrees to radians
+    theta = (float) Math.toRadians(theta % 360);
+
+    //
+    // Step 1 : Compute (x1', y1')
+    //
+    double x1prime = Math.cos(theta) * dx2 + Math.sin(theta) * dy2;
+    double y1prime = -Math.sin(theta) * dx2 + Math.cos(theta) * dy2;
+    // Ensure radii are large enough
+    rx = Math.abs(rx);
+    ry = Math.abs(ry);
+    double Prx = rx * rx;
+    double Pry = ry * ry;
+    double Px1 = x1prime * x1prime;
+    double Py1 = y1prime * y1prime;
+    double d = Px1 / Prx + Py1 / Pry;
+    if (d > 1) {
+      rx = Math.abs(Math.sqrt(d) * rx);
+      ry = Math.abs(Math.sqrt(d) * ry);
+      Prx = rx * rx;
+      Pry = ry * ry;
+    }
+
+    //
+    // Step 2 : Compute (cx', cy')
+    //
+    double sign = (largeArcFlag == sweepFlag) ? -1.0 : 1.0;
+    double coef = sign *
+      Math.sqrt(((Prx * Pry) - (Prx * Py1) - (Pry * Px1)) / ((Prx * Py1) + (Pry * Px1)));
+    double cxprime = coef * ((rx * y1prime) / ry);
+    double cyprime = coef * -((ry * x1prime) / rx);
+
+    //
+    // Step 3 : Compute (cx, cy) from (cx', cy')
+    //
+    double sx2 = (x0 + x2) / 2.0f;
+    double sy2 = (y0 + y2) / 2.0f;
+    double cx = sx2 + Math.cos(theta) * cxprime - Math.sin(theta) * cyprime;
+    double cy = sy2 + Math.sin(theta) * cxprime + Math.cos(theta) * cyprime;
+
+    //
+    // Step 4 : Compute the angleStart (theta1) and the angleExtent (dtheta)
+    //
+    double ux = (x1prime - cxprime) / rx;
+    double uy = (y1prime - cyprime) / ry;
+    double vx = (-x1prime - cxprime) / rx;
+    double vy = (-y1prime - cyprime) / ry;
+    double p, n;
+    // Compute the angle start
+    n = Math.sqrt((ux * ux) + (uy * uy));
+    p = ux; // (1 * ux) + (0 * uy)
+    sign = (uy < 0) ? -1.0 : 1.0;
+    double angleStart = Math.toDegrees(sign * Math.acos(p / n));
+    // Compute the angle extent
+    n = Math.sqrt((ux * ux + uy * uy) * (vx * vx + vy * vy));
+    p = ux * vx + uy * vy;
+    sign = (ux * vy - uy * vx < 0) ? -1.0 : 1.0;
+    double angleExtent = Math.toDegrees(sign * Math.acos(p / n));
+    if (!sweepFlag && angleExtent > 0) {
+      angleExtent -= 360;
+    } else if (sweepFlag && angleExtent < 0) {
+      angleExtent += 360;
+    }
+    angleExtent %= 360;
+    angleStart %= 360;
+
+    Arc2D.Float arc = new Arc2D.Float();
+    arc.x = (float) (cx - rx);
+    arc.y = (float) (cy - ry);
+    arc.width = (float) (rx * 2.0f);
+    arc.height = (float) (ry * 2.0f);
+    arc.start = (float) -angleStart;
+    arc.extent = (float) -angleExtent;
+
+    return Shape.convert(arc);
   }
 
   static List<Shape> absolute(SvgState state, List<Float> args) {
