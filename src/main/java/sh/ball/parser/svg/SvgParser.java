2172478acfeb2dd633f0763853f64fa936bcaa86
James Ball <james@ball.sh>
2021-05-09 20:00:46 +0100

    Add slider for vector cancelling

--- src/main/java/sh/ball/parser/svg/SvgParser.java
+++ src/main/java/sh/ball/parser/svg/SvgParser.java
@@ -15,7 +15,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import javax.xml.parsers.ParserConfigurationException;
