1919c4e4d25f5e71addc18a7cd913a5ceb6ec466
James Ball <james@ball.sh>
2021-06-15 22:02:29 +0100

    Update AudioEngine and add AudioDevice and AudioSample

--- src/main/java/sh/ball/audio/engine/AudioEngine.java
+++ src/main/java/sh/ball/audio/engine/AudioEngine.java
@@ -2,11 +2,14 @@ package sh.ball.audio.engine;
 
 import sh.ball.shapes.Vector2;
 
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.locks.ReentrantLock;
 
 public interface AudioEngine {
-  void play(Callable<Vector2> channelGenerator, ReentrantLock renderLock);
+  void play(Callable<Vector2> channelGenerator, ReentrantLock renderLock, AudioDevice device);
+
   void stop();
-  int sampleRate();
+
+  List<AudioDevice> devices();
 }
