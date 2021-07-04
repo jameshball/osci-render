a6c9b398e1ac59e96f4d1a73fb87c377bf34daa4
James Ball <james@ball.sh>
2021-07-04 18:40:22 +0100

    Add ConglomerateAudioEngine that combines both XtAudioEngine and JavaAudioEngine

--- src/main/java/sh/ball/gui/Gui.java
+++ src/main/java/sh/ball/gui/Gui.java
@@ -11,6 +11,7 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import sh.ball.audio.ShapeAudioPlayer;
+import sh.ball.audio.engine.ConglomerateAudioEngine;
 import sh.ball.audio.engine.JavaAudioEngine;
 import sh.ball.engine.Vector3;
 
@@ -24,7 +25,7 @@ public class Gui extends Application {
     System.setProperty("prism.lcdtext", "false");
 
     FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/osci-render.fxml"));
-    Controller controller = new Controller(new ShapeAudioPlayer(JavaAudioEngine::new));
+    Controller controller = new Controller(new ShapeAudioPlayer(ConglomerateAudioEngine::new));
     loader.setController(controller);
     Parent root = loader.load();
 
