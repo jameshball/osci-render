a15974481dc290270bd5623d0a1e13a4bb15270a
James Ball <james@ball.sh>
2022-10-31 20:35:48 +0000

    Allow text size to be specified using = at the start of a line for .txt files

--- src/main/java/sh/ball/parser/txt/TextParser.java
+++ src/main/java/sh/ball/parser/txt/TextParser.java
@@ -7,6 +7,8 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
@@ -17,11 +19,11 @@ import sh.ball.shapes.Shape;
 
 public class TextParser extends FileParser<FrameSource<List<Shape>>> {
 
-  private static final char WIDE_CHAR = 'W';
-  private static final double HEIGHT_SCALAR = 1.6;
   private final InputStream input;
   private final String familyName;
   private final int style;
+  Pattern textSizePattern = Pattern.compile("^\\$text_size=(\\d+)");
+  Pattern escapePattern = Pattern.compile("\\\\(.)");
 
   public TextParser(InputStream input, String familyName, int style) {
     this.input = input;
@@ -34,20 +36,36 @@ public class TextParser extends FileParser<FrameSource<List<Shape>>> {
     return "txt";
   }
 
+  public double getLineHeight(Font font, FontRenderContext frc) {
+    return font.getMaxCharBounds(frc).getHeight();
+  }
+
   @Override
   public FrameSource<List<Shape>> parse() throws IllegalArgumentException, IOException, ParserConfigurationException, SAXException {
     String text = new String(input.readAllBytes(), StandardCharsets.UTF_8);
     Font font = new Font(familyName, style, 100);
     FontRenderContext frc = new FontRenderContext(null, false, false);
-    GlyphVector tallChar = font.createGlyphVector(frc, String.valueOf(WIDE_CHAR));
-    double lineHeight = tallChar.getPixelBounds(frc, 0, 0).getHeight();
 
     List<Shape> shapes = new ArrayList<>();
     float yOffset = 0;
     for (String line : text.split("\n")) {
+      Matcher matcher = textSizePattern.matcher(line);
+
+      if (matcher.find()) {
+        float size = Float.parseFloat(matcher.group(1));
+        line = matcher.replaceFirst("");
+        font = font.deriveFont(size);
+      } else {
+        font = font.deriveFont((float) 100);
+      }
+
+      matcher = escapePattern.matcher(line);
+
+      line = matcher.replaceAll("$1");
+
       GlyphVector glyphVector = font.createGlyphVector(frc, line);
+      yOffset += getLineHeight(font, frc);
       shapes.addAll(Shape.convert(glyphVector.getOutline(0, yOffset)));
-      yOffset += lineHeight;
     }
 
     return new ShapeFrameSource((Shape.normalize(shapes)));
