c9a46c29d0ce97ffe54d5a67588fa77954dd7416
James Ball <james@ball.sh>
2023-07-30 14:01:56 +0100

    Completely remove custom SVG implementation and pugixml library in favour of JUCE classes

--- Source/svg/SvgParser.h
+++ Source/svg/SvgParser.h
@@ -2,21 +2,16 @@
 #include "../shape/Vector2.h"
 #include <JuceHeader.h>
 #include "../shape/Shape.h"
-#include "SvgState.h"
-#include "../xml/pugixml.hpp"
 
 class SvgParser {
 public:
 	SvgParser(juce::String svgFile);
 	~SvgParser();
 
+	static void pathToShapes(juce::Path& path, std::vector<std::unique_ptr<Shape>>& shapes);
+
 	std::vector<std::unique_ptr<Shape>> draw();
 private:
-	std::vector<std::string> preProcessPath(std::string path);
-	juce::String simplifyLength(juce::String length);
-	std::pair<double, double> getDimensions(pugi::xml_node&);
-	std::vector<std::unique_ptr<Shape>> parsePath(juce::String);
 	
 	std::vector<std::unique_ptr<Shape>> shapes;
-	SvgState state = SvgState();
 };
--- Source/svg/SvgState.cpp
+++
@@ -1,1 +0,0 @@
-#include "SvgState.h"
