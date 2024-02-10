575a2977d8d25fa2ba16b6bad86679a12dd1d99c
James Ball <james@ball.sh>
2024-02-10 12:18:05 +0000

    Add ls for debugging

--- ci/setup-env.sh
+++ ci/setup-env.sh
@@ -6,9 +6,12 @@ if [ $OS = "linux" ]; then
   sudo apt-get install clang git ladspa-sdk freeglut3-dev g++ libasound2-dev libcurl4-openssl-dev libfreetype6-dev libjack-jackd2-dev libx11-dev libxcomposite-dev libxcursor-dev libxinerama-dev libxrandr-dev mesa-common-dev webkit2gtk-4.0 juce-tools xvfb
 fi
 
+ls .
+
 ROOT=$(cd "$(dirname "$0")/.."; pwd)
 cd "$ROOT"
 echo "$ROOT"
+ls .
 rm -Rf bin
 mkdir bin
 
