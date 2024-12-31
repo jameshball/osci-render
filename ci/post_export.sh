aa6de051d9e7a8e9e960cd0cdc2e084440a7270f
James H Ball <james@ball.sh>
2024-12-31 15:46:13 +0000

    Remove All and Shared Code embed stages

--- ci/post_export.sh
+++ ci/post_export.sh
@@ -9,5 +9,5 @@ PROJECT_ROOT="$1"
 PBXPROJ_FILE="$2"
 PROJECT_NAME="$3"
 
-sh -c "$PROJECT_ROOT/ci/remove_embed_frameworks.sh \"$PBXPROJ_FILE\" \"$PROJECT_NAME\" \"VST3 Manifest Helper"\"
+sh -c "$PROJECT_ROOT/ci/remove_embed_frameworks.sh \"$PBXPROJ_FILE\" \"$PROJECT_NAME\" \"All"\"
 sh -c "$PROJECT_ROOT/ci/remove_embed_frameworks.sh \"$PBXPROJ_FILE\" \"$PROJECT_NAME\" \"Shared Code"\"
