e56043325641cf39d489a00779e8e3481cbd4bc9
James H Ball <james@ball.sh>
2025-01-05 16:30:36 +0000

    Change names of different build versions

--- ci/build.sh
+++ ci/build.sh
@@ -3,6 +3,8 @@
 PLUGIN="$1"
 VERSION="$2"
 
+OUTPUT_NAME="$PLUGIN-$VERSION"
+
 # If we are on the free version, we need to disable the build flag SOSCI_FEATURES
 if [ "$VERSION" = "free" ]; then
   # Edit the jucer file to disable the SOSCI_FEATURES flag
@@ -18,16 +20,16 @@ if [ "$OS" = "mac" ]; then
   cd "$ROOT/Builds/$PLUGIN/MacOSX"
   xcodebuild -configuration Release || exit 1
 
-  cp -R "$ROOT/Builds/$PLUGIN/MacOSX/build/Release/$PLUGIN.app" "$ROOT/ci/bin"
-  cp -R ~/Library/Audio/Plug-Ins/VST3/$PLUGIN.vst3 "$ROOT/ci/bin"
-  cp -R ~/Library/Audio/Plug-Ins/Components/$PLUGIN.component "$ROOT/ci/bin"
+  cp -R "$ROOT/Builds/$PLUGIN/MacOSX/build/Release/$PLUGIN.app" "$ROOT/ci/bin/$OUTPUT_NAME.app"
+  cp -R ~/Library/Audio/Plug-Ins/VST3/$PLUGIN.vst3 "$ROOT/ci/bin/$OUTPUT_NAME.vst3"
+  cp -R ~/Library/Audio/Plug-Ins/Components/$PLUGIN.component "$ROOT/ci/bin/$OUTPUT_NAME.component"
 
   cd "$ROOT/ci/bin"
   
-  zip -r ${PLUGIN}-mac.vst3.zip $PLUGIN.vst3
-  zip -r ${PLUGIN}-mac.component.zip $PLUGIN.component
-  zip -r ${PLUGIN}-mac.app.zip $PLUGIN.app
-  cp ${PLUGIN}*.zip "$ROOT/bin"
+  zip -r ${OUTPUT_NAME}-mac.vst3.zip $OUTPUT_NAME.vst3
+  zip -r ${OUTPUT_NAME}-mac.component.zip $OUTPUT_NAME.component
+  zip -r ${OUTPUT_NAME}-mac.app.zip $OUTPUT_NAME.app
+  cp ${OUTPUT_NAME}*.zip "$ROOT/bin"
 fi
 
 # Build linux version
@@ -35,13 +37,14 @@ if [ "$OS" = "linux" ]; then
   cd "$ROOT/Builds/$PLUGIN/LinuxMakefile"
   make CONFIG=Release
 
-  cp -r ./build/$PLUGIN.vst3 ./build/$PLUGIN "$ROOT/ci/bin"
+  cp -r ./build/$PLUGIN.vst3 "$ROOT/ci/bin/$OUTPUT_NAME.vst3"
+  cp -r ./build/$PLUGIN "$ROOT/ci/bin/$OUTPUT_NAME"
 
   cd "$ROOT/ci/bin"
 
-  zip -r ${PLUGIN}-linux-vst3.zip $PLUGIN.vst3
-  zip -r ${PLUGIN}-linux.zip $PLUGIN
-  cp ${PLUGIN}*.zip "$ROOT/bin"
+  zip -r ${OUTPUT_NAME}-linux-vst3.zip $OUTPUT_NAME.vst3
+  zip -r ${OUTPUT_NAME}-linux.zip $OUTPUT_NAME
+  cp ${OUTPUT_NAME}*.zip "$ROOT/bin"
 fi
 
 # Build Win version
@@ -54,9 +57,9 @@ if [ "$OS" = "win" ]; then
   cd "$ROOT/Builds/$PLUGIN/VisualStudio2022"
   "$MSBUILD_EXE" "$PLUGIN.sln" "//p:VisualStudioVersion=16.0" "//m" "//t:Build" "//p:Configuration=Release" "//p:Platform=x64" "//p:PreferredToolArchitecture=x64" "//restore" "//p:RestorePackagesConfig=true"
   cd "$ROOT/ci/bin"
-  cp "$ROOT/Builds/$PLUGIN/VisualStudio2022/x64/Release/Standalone Plugin/$PLUGIN.exe" "$ROOT/bin/$PLUGIN-win.exe"
-  cp "$ROOT/Builds/$PLUGIN/VisualStudio2022/x64/Release/Standalone Plugin/$PLUGIN.pdb" "$ROOT/bin/$PLUGIN-win.pdb"
-  cp -r "$ROOT/Builds/$PLUGIN/VisualStudio2022/x64/Release/VST3/$PLUGIN.vst3/Contents/x86_64-win/$PLUGIN.vst3" "$ROOT/bin/$PLUGIN-win.vst3"
+  cp "$ROOT/Builds/$PLUGIN/VisualStudio2022/x64/Release/Standalone Plugin/$PLUGIN.exe" "$ROOT/bin/$OUTPUT_NAME-win.exe"
+  cp "$ROOT/Builds/$PLUGIN/VisualStudio2022/x64/Release/Standalone Plugin/$PLUGIN.pdb" "$ROOT/bin/$OUTPUT_NAME-win.pdb"
+  cp -r "$ROOT/Builds/$PLUGIN/VisualStudio2022/x64/Release/VST3/$PLUGIN.vst3/Contents/x86_64-win/$PLUGIN.vst3" "$ROOT/bin/$OUTPUT_NAME-win.vst3"
 fi
 
 cd "$ROOT"
