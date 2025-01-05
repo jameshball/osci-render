aefb6adfe7583edf6dce79d0c3cd2803bb4f66a1
James H Ball <james@ball.sh>
2025-01-05 16:35:08 +0000

    Fix sed command

--- ci/build.sh
+++ ci/build.sh
@@ -8,7 +8,7 @@ OUTPUT_NAME="$PLUGIN-$VERSION"
 # If we are on the free version, we need to disable the build flag SOSCI_FEATURES
 if [ "$VERSION" = "free" ]; then
   # Edit the jucer file to disable the SOSCI_FEATURES flag
-  sed -i '' 's/SOSCI_FEATURES=1/SOSCI_FEATURES=0/' "$ROOT/$PLUGIN.jucer"
+  sed 's/SOSCI_FEATURES=1/SOSCI_FEATURES=0/' "$ROOT/$PLUGIN.jucer"
 fi
 
 # Resave jucer file
