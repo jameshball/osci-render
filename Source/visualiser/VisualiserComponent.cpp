64e7208feef69b45ffe8af82994f78970eaae81e
James H Ball <james@ball.sh>
2025-01-12 18:21:50 +0000

    Add space to pause and F11 to make visualiser fullscreen

--- Source/visualiser/VisualiserComponent.cpp
+++ Source/visualiser/VisualiserComponent.cpp
@@ -358,6 +358,14 @@ bool VisualiserComponent::keyPressed(const juce::KeyPress& key) {
             fullScreenCallback(FullScreenMode::MAIN_COMPONENT);
         }
         return true;
+    } else if (key.isKeyCode(juce::KeyPress::spaceKey)) {
+        setPaused(active);
+        return true;
+    } else if (key.isKeyCode(juce::KeyPress::F11Key)) {
+        // set fullscreen
+        juce::Component* component = juce::TopLevelWindow::getTopLevelWindow(0);
+        juce::Desktop::getInstance().setKioskModeComponent(component, true);
+        return true;
     }
 
     return false;
