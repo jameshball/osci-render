6d134c0403bf8eea7e40397b2628f8f4c515dd65
James H Ball <james@ball.sh>
2025-02-01 14:41:25 +0000

    Remove no_sanitize attribute

--- Source/visualiser/VisualiserComponent.cpp
+++ Source/visualiser/VisualiserComponent.cpp
@@ -174,7 +174,6 @@ void VisualiserComponent::mouseDoubleClick(const juce::MouseEvent& event) {
     }
 }
 
-__attribute__((no_sanitize("thread")))
 void VisualiserComponent::runTask(const std::vector<OsciPoint>& points) {
     {
         juce::CriticalSection::ScopedLockType lock(samplesLock);
