ca9613e56d30504987a481177e62780473fdfa8e
Anthony Hall <AnthonyHall.School@gmail.com>
2025-08-14 00:39:51 -0700

    Make dash frequency invariant and smoother

--- Source/audio/DashedLineEffect.h
+++ Source/audio/DashedLineEffect.h
@@ -1,34 +1,42 @@
 #pragma once
 #include <JuceHeader.h>
+#include "../PluginProcessor.h"
 
 class DashedLineEffect : public osci::EffectApplication {
 public:
-	osci::Point apply(int index, osci::Point vector, const std::vector<std::atomic<double>>& values, double sampleRate) override {
-		// dash length in seconds
-		double dashLength = values[0] / 400;
-		int dashLengthSamples = (int)(dashLength * sampleRate);
-		dashLengthSamples = juce::jmin(dashLengthSamples, MAX_BUFFER);
-		
-		if (dashIndex >= dashLengthSamples) {
-			dashIndex = 0;
+	DashedLineEffect(OscirenderAudioProcessor& p) : audioProcessor(p) {}
+
+	osci::Point apply(int index, osci::Point input, const std::vector<std::atomic<double>>& values, double sampleRate) override {
+		double dashCount = juce::jmax(1.0, values[0].load()); // Dashes per cycle
+		double dashCoverage = juce::jlimit(0.0, 1.0, values[1].load());
+		double dashOffset = values[2];
+		double dashLengthSamples = (sampleRate / audioProcessor.frequency) / dashCount;
+		double dashPhase = framePhase * dashCount - dashOffset;
+		dashPhase = dashPhase - std::floor(dashPhase); // Wrap
+		buffer[bufferIndex] = input;
+
+		// Linear interpolation works much better than nearest for this
+		double samplePos = bufferIndex - dashLengthSamples * dashPhase * (1 - dashCoverage);
+		samplePos = samplePos - buffer.size() * std::floor(samplePos / buffer.size()); // Wrap to [0, size]
+		int lowIndex = (int)std::floor(samplePos) % buffer.size();
+		int highIndex = (lowIndex + 1) % buffer.size();
+		double mixFactor = samplePos - std::floor(samplePos); // Fractional part
+		osci::Point output = (1 - mixFactor) * buffer[lowIndex] + mixFactor * buffer[highIndex];
+
+		bufferIndex++;
+		if (bufferIndex >= buffer.size()) {
 			bufferIndex = 0;
 		}
+		framePhase += audioProcessor.frequency / sampleRate;
+		framePhase = framePhase - std::floor(framePhase);
 
-		buffer[bufferIndex] = vector;
-		bufferIndex++;
-		
-		vector = buffer[dashIndex];
-		
-		if (index % 2 == 0) {
-			dashIndex++;
-		}
-		
-		return vector;
+		return output;
 	}
 
 private:
+	OscirenderAudioProcessor &audioProcessor;
 	const static int MAX_BUFFER = 192000;
 	std::vector<osci::Point> buffer = std::vector<osci::Point>(MAX_BUFFER);
-	int dashIndex = 0;
 	int bufferIndex = 0;
+	double framePhase = 0.0; // [0, 1]
 };
