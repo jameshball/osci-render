7bb2a676c4dc9ec200c5c8c48345d8aa29615d70
DJLevel3 <zachcommercialmail@gmail.com>
2025-08-16 10:28:55 -0600

    Make compilation work out of the box on MacOS by updating LuaJIT script and removing hardcoded developer ID

--- luajit_linux_macos.sh
+++ luajit_linux_macos.sh
@@ -3,5 +3,10 @@
 # Run this if you're on Linux and you haven't already built LuaJIT.
 # If you don't, osci-render won't compile.
 
-cd modules/LuaJIT/src
-BUILDMODE=static make -j$(nproc)
+cd "$DIR/modules/LuaJIT/src"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  make -j$(sysctl -n hw.logicalcpu) BUILDMODE=static MACOSX_DEPLOYMENT_TARGET=10.13
+else
+  make -j$(nproc) BUILDMODE=static
+fi
