b7e2faaaf2bf3d9d3d42313c10656deb2e71504c
DJLevel3 <zachcommercialmail@gmail.com>
2025-08-17 09:13:15 -0600

    why do quotes hate meeee

--- ci/build.sh
+++ ci/build.sh
@@ -45,9 +45,8 @@ if [ "$OS" = "win" ]; then
   VS_WHERE="C:/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe"
   
   VS_PATH=$("$VS_WHERE" -latest -property installationPath)
-  DEVCMD_BAT="$VS_PATH\\VC\\Auxiliary\\Build\\vcvars64.bat"
   
-  eval "$($(cygpath "$COMSPEC") /c$(cygpath -w "$ROOT/ci/vcvars_export.bat") "$(echo $DEVCMD_BAT)" "$(cygpath -w "$SHELL")")"
+  eval "$($(cygpath "$COMSPEC") /c$(cygpath -w "$ROOT/ci/vcvars_export.bat") "$(echo $VS_PATH)\\VC\\Auxiliary\\Build\\vcvars64.bat" "$(cygpath -w "$SHELL")")"
   
   MSBUILD_EXE=$("$VS_WHERE" -latest -requires Microsoft.Component.MSBuild -find "MSBuild\**\Bin\MSBuild.exe")
   echo $MSBUILD_EXE
