5659f1234ed309b179d55a54bfc9bf0cf2af1392
DJLevel3 <zachcommercialmail@gmail.com>
2025-08-17 11:11:32 -0600

    use 64 bit

--- ci/vcvars_export.bat
+++ ci/vcvars_export.bat
@@ -3,5 +3,5 @@
 set VSWHERE="C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere"
 for /f "tokens=*" %%i in ('%VSWHERE% -latest -property installationPath') do set VSWHERE2=%%i
 
-call "%VSWHERE2%\Common7\Tools\VsDevCmd.bat" > nul
+call "%VSWHERE2%\VC\Auxiliary\Build\VCVars64.bat" > nul
 bash -c "export -p"
