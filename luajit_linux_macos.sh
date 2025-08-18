23e8f5d2fa78ead3c41018b513c684739a084820
DJLevel3 <zachcommercialmail@gmail.com>
2025-08-18 12:36:10 -0600

    revert relative directory for now

--- luajit_linux_macos.sh
+++ luajit_linux_macos.sh
@@ -3,7 +3,7 @@
 # Run this if you're on Linux or macOS and you haven't already built LuaJIT.
 # If you don't, osci-render won't compile.
 
-cd "../../../modules/LuaJIT/src" || exit 1
+cd "$DIR/modules/LuaJIT/src" || exit 1
 
 if [[ "$OSTYPE" == *"darwin"* ]]; then
   echo "Building LuaJIT universal binary (x86_64 + arm64)..."
