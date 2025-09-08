78a5be15b1f6899380adae668b7c2b9edfa098da
Anthony Hall <AnthonyHall.School@gmail.com>
2025-09-08 03:51:32 -0700

    Kaleidoscope + defaults, todo comments

--- Source/audio/KaleidoscopeEffect.h
+++ Source/audio/KaleidoscopeEffect.h
@@ -7,8 +7,9 @@ public:
     KaleidoscopeEffect(OscirenderAudioProcessor& p) : audioProcessor(p) {}
 
     osci::Point apply(int index, osci::Point input, const std::vector<std::atomic<double>>& values, double sampleRate) override {
+        // TODO comment
         const double twoPi = juce::MathConstants<double>::twoPi;
-        double copies = juce::jmax(1.0, values[0].load());
+        double copies = juce::jmax(2.0, values[0].load());
         bool mirror = (bool)(values[1].load() > 0.5);
         double spread = juce::jlimit(0.0, 1.0, values[2].load());
         double clip = juce::jlimit(0.0, 1.0, values[3].load());
@@ -16,16 +17,13 @@ public:
 
         // Ensure values extremely close to integer don't get rounded up
         double fractionalPart = copies - std::floor(copies);
-        double ceilCopies = fractionalPart > 1e-4 ? std::ceil(copies) : std::floor(copies);
+        double ceilCopies = fractionalPart > 1e-3 ? std::ceil(copies) : std::floor(copies);
 
         double currentRegion = std::floor(framePhase * copies);
         double polarity = (int)currentRegion % 2 == 0 ? 1 : -1;
 
-        // TODO try splitting midway
-        double theta = currentRegion / copies * twoPi + angleOffset;
-        double rotTheta = theta;
-        osci::Point output((1 - spread) * input.x + spread, (1 - spread) * input.y, input.z);
-        output = osci::Point(output.y, -output.x, output.z);
+        input = osci::Point(input.y, -input.x, input.z);
+        osci::Point output((1 - spread) * input.x + spread, (1 - spread) * input.y, (1 - spread) * input.z);
         if (mirror && (int)currentRegion % 2 == 1) {
             output.y = -output.y;
         }
@@ -34,17 +32,15 @@ public:
         osci::Point upperPlaneNormal(std::sin(0.5 * regionSize), -std::cos(0.5 * regionSize), 0);
         osci::Point lowerPlaneNormal(upperPlaneNormal.x, -upperPlaneNormal.y, 0);
         osci::Point clippedOutput = clipToPlane(output, upperPlaneNormal);
-
-        clippedOutput = clipToPlane(output, lowerPlaneNormal);
+        clippedOutput = clipToPlane(clippedOutput, lowerPlaneNormal);
         if (clippedOutput.x < 0) {
             clippedOutput.x = 0;
             clippedOutput.y = 0;
         }
-
         output = (1 - clip) * output + clip * clippedOutput;
 
-        //osci::Point offset(std::cos(theta), std::sin(theta), 0.0);
 
+        double rotTheta = (currentRegion / copies) * twoPi + angleOffset;
         output.rotate(0, 0, rotTheta);
 
         framePhase += audioProcessor.frequency / ceilCopies / sampleRate;
@@ -60,18 +56,18 @@ public:
                 // TODO ID strings
                 new osci::EffectParameter("Copies",
                                           "Controls the number of copies of the input shape to draw. Splitting the shape into multiple copies creates audible harmony.",
-                                          "kaleidoscopeCopies", VERSION_HINT, 3.0, 1.0, 6.0),
+                                          "kaleidoscopeCopies", VERSION_HINT, 6.0, 2.0, 10.0),
                 new osci::EffectParameter("Mirror",
                                           "Controls how much the input shape is scaled in the kaleidoscope image.",
-                                          "kaleidoscopeMirror", VERSION_HINT, 0.0, 0.0, 1.0),
+                                          "kaleidoscopeMirror", VERSION_HINT, 1.0, 0.0, 1.0),
                 new osci::EffectParameter("Spread",
-                                          "Controls how much the input shape is scaled in the kaleidoscope image.",
-                                          "kaleidoscopeSpread", VERSION_HINT, 0.5, 0.0, 1.0),
+                                          "Controls how .",
+                                          "kaleidoscopeSpread", VERSION_HINT, 0.4, 0.0, 1.0),
                 //new osci::EffectParameter("Offset",
                 //                          "Controls how much the input shape is scaled in the kaleidoscope image.",
                 //                          "harmonicDuplicatorSpread", VERSION_HINT, 0.5, 0.0, 1.0),
                 new osci::EffectParameter("Clip",
-                                          "Controls whether each copy of the shape is clipped within its own region.",
+                                          "Clips each copy of the shape within its own region.",
                                           "kaleidoscopeClip", VERSION_HINT, 1.0, 0.0, 1.0),
                 new osci::EffectParameter("Angle Offset",
                                           "Rotates the offsets between copies without rotating the input shape.",
