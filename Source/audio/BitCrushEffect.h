26f67c6297d7d9d0d62a234295f110d845fb708a
Anthony Hall <AnthonyHall.School@gmail.com>
2025-09-08 17:40:46 -0700

    Round instead of truncate, add dry/wet

--- Source/audio/BitCrushEffect.h
+++ Source/audio/BitCrushEffect.h
@@ -5,7 +5,8 @@ class BitCrushEffect : public osci::EffectApplication {
 public:
 	// algorithm from https://www.kvraudio.com/forum/viewtopic.php?t=163880
 	osci::Point apply(int index, osci::Point input, const std::vector<std::atomic<double>>& values, double sampleRate) override {
-		double value = values[0];
+		double effectScale = juce::jlimit(0.0, 1.0, values[0].load());
+        double value = values[1].load();
 		// change rage of value from 0-1 to 0.0-0.78
 		double rangedValue = value * 0.78;
 		double powValue = pow(2.0f, 1.0 - rangedValue) - 1.0;
@@ -13,13 +14,19 @@ public:
 		double x = powf(2.0f, crush);
 		double quant = 0.5 * x;
 		double dequant = 1.0f / quant;
-		return osci::Point(dequant * (int)(input.x * quant), dequant * (int)(input.y * quant), dequant * (int)(input.z * quant));
+		osci::Point output(dequant * std::round(input.x * quant),
+                           dequant * std::round(input.y * quant),
+                           dequant * std::round(input.z * quant));
+        return (1 - effectScale) * input + effectScale * output;
 	}
 
 	std::shared_ptr<osci::Effect> build() const override {
-		auto eff = std::make_shared<osci::Effect>(
-			std::make_shared<BitCrushEffect>(),
-			new osci::EffectParameter("Bit Crush", "Limits the resolution of points drawn to the screen, making the object look pixelated, and making the audio sound more 'digital' and distorted.", "bitCrush", VERSION_HINT, 0.7, 0.0, 1.0));
+        auto eff = std::make_shared<osci::Effect>(
+            std::make_shared<BitCrushEffect>(),
+            std::vector<osci::EffectParameter*>{
+                new osci::EffectParameter("Bit Crush", "Limits the resolution of points drawn to the screen, making the object look pixelated, and making the audio sound more 'digital' and distorted.", "bitCrushEnable", VERSION_HINT, 0.7, 0.0, 1.0),
+                new osci::EffectParameter("Bit Crush Strength", "Controls the resolution which points are limited to.", "bitCrushStrength", VERSION_HINT, 0.7, 0.0, 1.0)
+        });
 		eff->setIcon(BinaryData::bitcrush_svg);
 		return eff;
 	}
