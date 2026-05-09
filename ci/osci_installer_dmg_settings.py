9f9199132d4a68811f7ebb1aaff09fc427259661
James H Ball <james@ball.sh>
2026-05-09 10:05:41 +0100

    Fix installer DMG volume icon visibility

--- ci/osci_installer_dmg_settings.py
+++ ci/osci_installer_dmg_settings.py
@@ -1,9 +1,11 @@
 application = defines["app"]  # noqa: F821
 volume_icon = defines["icon"]  # noqa: F821
 app_filename = "osci-installer.app"
+volume_icon_filename = ".VolumeIcon.icns"
 
 files = [(application, app_filename)]
 symlinks = {}
+hide = [volume_icon_filename]
 hide_extensions = [app_filename]
 
 icon = volume_icon
@@ -29,4 +31,7 @@ scroll_position = (0, 0)
 label_pos = "bottom"
 text_size = 13
 icon_size = 128
-icon_locations = {app_filename: (260, 230)}
+icon_locations = {
+    app_filename: (260, 230),
+    volume_icon_filename: (260, 900),
+}
