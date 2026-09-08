8d0bb5b96a591e6449f3ccfcd3dd041f74b21230
James H Ball <james@ball.sh>
2026-09-08 19:59:27 +0100

    Add braces to frame length guard

--- Source/audio/synth/ShapeVoice.cpp
+++ Source/audio/synth/ShapeVoice.cpp
@@ -454,7 +454,9 @@ void ShapeVoice::renderNextBlock(juce::AudioSampleBuffer& outputBuffer, int star
                 if (currentSound->updateFrame(frame)) {
                     double prevFrameLength = frameLength;
                     frameLength = currentSound->getFrameLength();
-                    if (frameLength > 0 && prevFrameLength > 0) frameDrawn *= frameLength / prevFrameLength;
+                    if (frameLength > 0 && prevFrameLength > 0) {
+                        frameDrawn *= frameLength / prevFrameLength;
+                    }
                 }
             }
             shapeDrawn = frameDrawn;
